865518d7ed8175b1c60f4fa32f26c04686b28f30
Cleaza <107327396+CleazaMain@users.noreply.github.com>
2023-11-11 17:34:16 +0300

    Update wartycoon.lua

--- wartycoon.lua
+++ wartycoon.lua
@@ -1,20 +1,19 @@
-if wartn_LOADED and not _G.wartn_DEBUG == true then
-	-- error(War Tycoon, 0)
-	return
+local genv = getgenv()
+
+if genv.AlreadyExecuted then
+    return Notification(1, "SmellLikehacker Hub", "AlreadyExecuted", 5)
 end
 
-pcall(function() getgenv().wartn_LOADED = true end)
+genv.AlreadyExecuted = true
 
-if getgenv().AlreadyExecuted then
-	return Notification(1, "SmellLikehacker Hub", "AlreadyExecuted",5)
+genv.wartn = function()
+    local queue_on_teleport = (syn and syn.queue_on_teleport) or queue_on_teleport or queueonteleport
+    if queue_on_teleport then
+        pcall(function()
+            queue_on_teleport('loadstring(game:HttpGet"https://raw.githubusercontent.com/SmellLikeHacker/MyEdit/main/Hub")()')
+        end)
+    end
 end
 getgenv().AlreadyExecuted = true
-task.spawn(function()
-	local queue_on_teleport = (syn and syn.queue_on_teleport) or queue_on_teleport or queueonteleport
-	if queue_on_teleport then
-		pcall(function()
-			queue_on_teleport('loadstring(game:HttpGet"https://raw.githubusercontent.com/SmellLikeHacker/MyEdit/main/Hub")()')
-		end)
-	end
-end)
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SmellLikeHacker/IDK/main/!Map"))()
